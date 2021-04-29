1c12b7e33a424e58b10908701fe259715804e889
Raul Duarte <raul.duarte@mercafacil.me>
2021-04-29 16:26:06 -0300

    refactor: buildSchema update

--- src/utils/buildSchema.ts
+++ src/utils/buildSchema.ts
@@ -1,11 +1,13 @@
-// import { buildSchema, BuildSchemaOptions } from 'type-graphql'
-// import Container from 'typedi'
-// import { ObjectId } from 'mongodb'
-// import { ObjectIdScalar } from './'
+import { buildSchema } from 'type-graphql'
+import Container from 'typedi'
+import { ObjectId } from 'mongodb'
+import { ObjectIdScalar } from './'
+import { resolvers } from '../modules'
+import { GraphQLSchema } from 'graphql/type'
 
-// export const gqlBuildSchema = (): BuildSchemaOptions =>
-//   buildSchema({
-//     resolvers: null,
-//     container: Container,
-//     scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }]
-//   })
+export const gqlbuildSchema = async (): Promise<GraphQLSchema> =>
+  await buildSchema({
+    resolvers,
+    container: Container,
+    scalarsMap: [{ type: ObjectId, scalar: ObjectIdScalar }]
+  })
